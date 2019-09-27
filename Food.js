9e3884935e9db573b109e2ac669589d5a7b9dff0
Krys <21113012+Krystade@users.noreply.github.com>
2019-09-27 03:10:08 -0700

    Removed all unnecessary semi colons

--- Food.js
+++ Food.js
@@ -1,12 +1,12 @@
 function Food (x,y){
-	this.x = x;
-	this.y = y;
+	this.x = x
+	this.y = y
 	this.size = 15
 	this.display = function(){
-		noStroke();
-		textAlign(CENTER);
-		textSize(20);
-		fill(20,200,40);
-		ellipse(this.x,this.y, this.size, this.size);
+		noStroke()
+		textAlign(CENTER)
+		textSize(20)
+		fill(20,200,40)
+		ellipse(this.x,this.y, this.size, this.size)
 	}
 }
