026ef3e0760c353fdeb51bdab9e6e4b7ff191b8c
Krys <21113012+Krystade@users.noreply.github.com>
2019-09-30 23:06:33 -0700

    Update Food.js
    added new function for determining which sector the food item is in

--- Food.js
+++ Food.js
@@ -1,12 +1,23 @@
 function Food (x,y){
 	this.x = x
 	this.y = y
-	this.size = 15
+	this.size = 50
+	
 	this.display = function(){
 		noStroke()
 		textAlign(CENTER)
 		textSize(20)
 		fill(20,200,40)
 		ellipse(this.x,this.y, this.size, this.size)
+	}
+	
+	/*=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=*/
+	
+	this.isInside = function(dimensions){
+		if(this.x >= dimensions[0] && this.x < dimensions[1] && this.y >= dimensions[2] && this.y < dimensions[3]){
+			return true
+		}else{
+			return false
+		}
 	}
 }
