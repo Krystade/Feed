dba00db3a6840acd84fd914aa94e826931d9f66d
Krys <21113012+Krystade@users.noreply.github.com>
2019-11-11 18:24:03 -0800

    Update Menu.js
    adding more options

--- Menu.js
+++ Menu.js
@@ -3,7 +3,7 @@ function Menu (x, y) {
 	this.x = x
 	this.y = y
 	// For the menu itself
-	this.width = 140
+	this.width = 140*2
 	this.height = 350
 	this.size = [50,20]
 	
@@ -14,6 +14,7 @@ function Menu (x, y) {
 	menuButton.size(this.size)
 	
 	this.display = function(){
+		push()
 		stroke(1)
 		textAlign(CENTER)
 		textSize(20)
@@ -28,6 +29,22 @@ function Menu (x, y) {
 			for(var i = 0; i < 3; i++){
 				rect(20, this.y + this.size + 30 * i, 10, 10)
 			}
+			textAlign(CENTER)
+			fill(0)
+			noStroke()
+			textSize(15)
+			text("Click Mode:", this.x + this.width/2, this.y + 20 + 30*4)
+			text("Place Mode:", this.x + this.width/2, this.y + 30*6)
+			text("Color Mode:", this.x + this.width/2, this.y + 10 + 30*7)
+			if(colorRadio.value() == 'Custom'){
+				text("R, G, B:", this.x + this.width/2 - 65, this.y + 10 + 30*8)
+				colorInput.show()
+			}else{
+				colorInput.hide()
+			}
+			text("Create            mobs            ", this.x + this.width/2, this.y + 30*9)
+			text("Create            foods            ", this.x + this.width/2, this.y + 30*10)	
+			
 			//Show all the buttons if the menu is open
 			for(var i = 0; i < buttons.length; i++){
 				buttons[i].show()
@@ -39,5 +56,6 @@ function Menu (x, y) {
 			}
 			
 		}
+		pop()
 	}
 }
