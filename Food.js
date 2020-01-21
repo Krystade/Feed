fdfa7eb9613c53008f356c97d796eb892d2f463c
Krys <21113012+Krystade@users.noreply.github.com>
2020-01-21 01:56:23 -0800

    Slight optimization
    Calculate sector once upon creation

--- Food.js
+++ Food.js
@@ -6,23 +6,22 @@ function Food (x,y){
 	this.size = 50
 	this.value = 5
 	this.color = color(20, 200, 40)
-	this.sector = [-1, -1]
+	
+	this.sector = calcSector(this.x, this.y)
+	sectors[this.sector[0]][this.sector[1]].push(this)
 	/*=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=*/
 	
 	this.display = function(){
-		if(this.sector[0] == -1){
-			// Assign the correct sector
-			this.sector = calcSector(this.x, this.y)
-			// Push food into sector array
-			sectors[this.sector[0]][this.sector[1]].push(this)
+		// Check if the food is on screen before displaying it
+		if(this.x + this.size/2 > -trans[0] && this.x - this.size/2 < windowWidth/scaleNum - trans[0] && this.y + this.size/2 > -trans[1] && this.y - this.size/2 < windowWidth/scaleNum - trans[1]){
+			push()
+			noStroke()
+			textAlign(CENTER)
+			textSize(20)
+			fill(20, 200, 40)
+			ellipse(this.x,this.y, this.size, this.size)
+			pop()
 		}
-		push()
-		noStroke()
-		textAlign(CENTER)
-		textSize(20)
-		fill(20, 200, 40)
-		ellipse(this.x,this.y, this.size, this.size)
-		pop()
 	}
 	/*=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=*/
 }
